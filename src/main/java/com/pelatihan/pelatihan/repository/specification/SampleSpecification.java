27de524004a0615a2ce81ce6eb7931b058ae6d1c
Arya Segara <aryasegara10@gmail.com>
2025-01-12 14:04:57 +0700

    pertemuan-5

--- src/main/java/com/pelatihan/pelatihan/repository/specification/SampleSpecification.java
+++ src/main/java/com/pelatihan/pelatihan/repository/specification/SampleSpecification.java
@@ -1,5 +1,22 @@
 package com.pelatihan.pelatihan.repository.specification;
 
+import org.springframework.data.jpa.domain.Specification;
+
+import com.pelatihan.pelatihan.model.Sample;
+
 public class SampleSpecification {
+    public static Specification<Sample> containName(String name){
+        return (root,query,builder) -> 
+                builder.like(root.get("name"), "%" + name + "%");
+    }
     
+    public static Specification<Sample> equalCode(String code){
+        return (root,query,builder) -> 
+                builder.equal(root.get("code"), code);
+    }
+
+    public static Specification<Sample> equalStatus(Boolean status){
+        return (root,query,builder) -> 
+                builder.equal(root.get("status"), status);
+    }
 }
