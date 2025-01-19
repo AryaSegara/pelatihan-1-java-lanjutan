e1a2e8288ee5cfbbf3842aa8d346748dcb0564bf
Arya Segara <aryasegara10@gmail.com>
2025-01-19 13:57:36 +0700

    pertemuan-5

--- src/main/java/com/pelatihan/pelatihan/service/UserServiceImpl.java
+++ src/main/java/com/pelatihan/pelatihan/service/UserServiceImpl.java
@@ -2,13 +2,32 @@ package com.pelatihan.pelatihan.service;
 
 import java.time.LocalDate;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pelatihan.pelatihan.dto.RegisterUserDto;
 import com.pelatihan.pelatihan.model.Users;
+import com.pelatihan.pelatihan.repository.RoleRepository;
+import com.pelatihan.pelatihan.repository.UserRoleRepository;
+import com.pelatihan.pelatihan.repository.UsersRepository;
 
 @Service
 public class UserServiceImpl implements UserService{
+
+    private final UsersRepository usersRepository;
+    private final RoleRepository roleRepository;
+    private final UserRoleRepository userRoleRepository;
+
+    @Autowired
+    UserServiceImpl(UsersRepository usersRepository,
+                    RoleRepository roleRepository,
+                    UserRoleRepository userRoleRepository){
+
+        this.usersRepository = usersRepository;
+        this.roleRepository = roleRepository;
+        this.userRoleRepository = userRoleRepository;
+    }
+
 
     @Override
     public void register(RegisterUserDto dto){
