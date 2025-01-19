7a30df03802db2e146780abefb350c8b5eae1be7
Arya Segara <aryasegara10@gmail.com>
2025-01-19 14:48:06 +0700

    pertemuan-5

--- src/main/java/com/pelatihan/pelatihan/controller/UsersController.java
+++ src/main/java/com/pelatihan/pelatihan/controller/UsersController.java
@@ -3,10 +3,10 @@ package com.pelatihan.pelatihan.controller;
 import com.pelatihan.pelatihan.dto.GenericResponse;
 import com.pelatihan.pelatihan.dto.RegisterUserDto;
 import com.pelatihan.pelatihan.service.UserService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
