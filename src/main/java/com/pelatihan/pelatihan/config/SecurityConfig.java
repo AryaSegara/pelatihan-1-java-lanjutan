0c2df680d619fc91c6cdb9c3e8db26d25841dd04
Arya Segara <aryasegara10@gmail.com>
2025-02-09 10:52:35 +0700

    pertemuan-8

--- src/main/java/com/pelatihan/pelatihan/config/SecurityConfig.java
+++ src/main/java/com/pelatihan/pelatihan/config/SecurityConfig.java
@@ -1,5 +1,21 @@
 package com.pelatihan.pelatihan.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
-    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> 
+                            auth.requestMatchers("/login")
+                            .permitAll()
+                            .anyRequest()
+                            .authenticated())
+                .build();
+    }
 }
