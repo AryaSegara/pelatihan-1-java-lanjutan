e4d2ca9f9b07a88c597a4d62d11856445fe88d09
Arya Segara <aryasegara10@gmail.com>
2025-02-09 11:14:00 +0700

    pertemuan-8

--- src/main/java/com/pelatihan/pelatihan/service/LoginServiceImpl.java
+++ src/main/java/com/pelatihan/pelatihan/service/LoginServiceImpl.java
@@ -1,9 +1,11 @@
 package com.pelatihan.pelatihan.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pelatihan.pelatihan.dto.LoginDto;
 import com.pelatihan.pelatihan.dto.UserDetailDto;
+import com.pelatihan.pelatihan.repository.UsersRepository;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,9 +13,18 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class LoginServiceImpl implements LoginService{
 
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public LoginServiceImpl(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
     @Override
     public UserDetailDto login(LoginDto dto) {
         return null;
     }
+
+
     
 }
