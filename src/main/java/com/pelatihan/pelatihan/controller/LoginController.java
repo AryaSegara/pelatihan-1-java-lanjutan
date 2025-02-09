f7e30eddef095b2d798cf81e05a2243fea6a5735
Arya Segara <aryasegara10@gmail.com>
2025-02-09 11:00:51 +0700

    pertemuan-8

---
+++ src/main/java/com/pelatihan/pelatihan/controller/LoginController.java
@@ -0,0 +1,36 @@
+package com.pelatihan.pelatihan.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.pelatihan.pelatihan.dto.GenericResponse;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+    
+    @PostMapping
+    public ResponseEntity<Object> login(){
+        try {
+            
+        } catch (ResponseStatusException rse) {
+            return ResponseEntity.status(rse.getStatusCode())
+                                        .body(GenericResponse.builder()
+                                        .success(false)
+                                        .message(rse.getReason())
+                                        .data(null)
+                                        .build());
+        }catch(Exception e){
+            return ResponseEntity.internalServerError()
+                                        .body(GenericResponse.builder()
+                                        .success(false)
+                                        .message("Internal Server Error")
+                                        .data(null)
+                                        .build());
+        }
+        return null;
+    }
+}
