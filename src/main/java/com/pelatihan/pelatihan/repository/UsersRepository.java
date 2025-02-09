d5c0090f0471f5e9c2e7c4f8bd16fb3e9c9d6c20
Arya Segara <aryasegara10@gmail.com>
2025-02-09 11:16:09 +0700

    pertemuan-8

--- src/main/java/com/pelatihan/pelatihan/repository/UsersRepository.java
+++ src/main/java/com/pelatihan/pelatihan/repository/UsersRepository.java
@@ -1,9 +1,11 @@
 package com.pelatihan.pelatihan.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pelatihan.pelatihan.model.Users;
 
 public interface UsersRepository extends JpaRepository <Users, Integer>{
-    
+    Optional<Users> findByUsername(String username);
 }
--- src/main/java/com/pelatihan/pelatihan/service/LoginServiceImpl.java
+++ src/main/java/com/pelatihan/pelatihan/service/LoginServiceImpl.java
@@ -1,10 +1,13 @@
 package com.pelatihan.pelatihan.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pelatihan.pelatihan.dto.LoginDto;
 import com.pelatihan.pelatihan.dto.UserDetailDto;
+import com.pelatihan.pelatihan.model.Users;
 import com.pelatihan.pelatihan.repository.UsersRepository;
 
 import lombok.extern.slf4j.Slf4j;
@@ -22,6 +25,7 @@ public class LoginServiceImpl implements LoginService{
 
     @Override
     public UserDetailDto login(LoginDto dto) {
+        Optional<Users> optionalUsers = usersRepository.findByUsername(dto.getUsername());
         return null;
     }
 
