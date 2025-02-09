2c07d361ae85cde6c1f57f2f7b3ddb783c1a5fa8
Arya Segara <aryasegara10@gmail.com>
2025-02-09 11:10:15 +0700

    pertemuan-8

--- src/main/java/com/pelatihan/pelatihan/service/LoginService.java
+++ src/main/java/com/pelatihan/pelatihan/service/LoginService.java
@@ -1,5 +1,8 @@
 package com.pelatihan.pelatihan.service;
 
+import com.pelatihan.pelatihan.dto.LoginDto;
+import com.pelatihan.pelatihan.dto.UserDetailDto;
+
 public interface LoginService {
-    
+    UserDetailDto login(LoginDto dto);
 }
