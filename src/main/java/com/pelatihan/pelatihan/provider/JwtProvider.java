95d80e34f27a7335fb600c5207549b9fb3665f59
Arya Segara <aryasegara10@gmail.com>
2025-02-16 11:13:41 +0700

    pertemuan-9

--- src/main/java/com/pelatihan/pelatihan/provider/JwtProvider.java
+++ src/main/java/com/pelatihan/pelatihan/provider/JwtProvider.java
@@ -4,19 +4,27 @@ import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import io.jsonwebtoken.*;
 
 public class JwtProvider {
     
-    public String generateToken(String userId, String email, String role) {
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.access-token-validity}")
+    private Long accessTokenValidity;
+
+    public String generateToken(String userId, String username, String role) {
 
         Claims claims = Jwts.claims().setSubject(userId);
         claims.setId(userId);
-        claims.put("email", email);
+        claims.put("email", username);
         claims.put("authorities", List.of(role));
 
         Date tokenCreateTime = new Date();
-        Date tokenValidity = new Date(tokenCreateTime.getTime() + TimeUnit.MINUTES.toMillis(accessTokenValidity);
+        Date tokenValidity = new Date(tokenCreateTime.getTime() + TimeUnit.MINUTES.toMillis(accessTokenValidity));
 
         return Jwts.builder()
                     .setClaims(claims)
@@ -24,5 +32,6 @@ public class JwtProvider {
                     .signWith(SignatureAlgorithm.HS512, secretKey)
                     .compact();
     }
-    
+
+
 }
