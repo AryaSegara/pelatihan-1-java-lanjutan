f8fb6a106bf0a5a0e4c5f97f5d8acbc66af0cefe
Arya Segara <aryasegara10@gmail.com>
2025-03-09 11:06:17 +0700

    pertemuan-12

--- src/main/java/com/pelatihan/pelatihan/service/EmailServiceImpl.java
+++ src/main/java/com/pelatihan/pelatihan/service/EmailServiceImpl.java
@@ -1,5 +1,27 @@
 package com.pelatihan.pelatihan.service;
 
-public class EmailServiceImpl {
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@Service
+public class EmailServiceImpl implements EmailService{
+
+    @Autowired
+    private JavaMailSender JavaMailSender;
+
+    public EmailServiceImpl(JavaMailSender javaMailSender){
+        this.javaMailSender = javaMailSender;
+    }
+
+    
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage(); 
+        message.setFrom("noreply@baeldung.com");
+        message.setTo(to); 
+        message.setSubject(subject); 
+        message.setText(body);
+        javaMailSender.send(message);
+    }
     
 }
