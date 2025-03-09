19de44bd22945c197479a3f74a9ce9d8b00009e8
Arya Segara <aryasegara10@gmail.com>
2025-03-09 10:32:40 +0700

    pertemuan-12

--- src/main/java/com/pelatihan/pelatihan/config/EmailConfig.java
+++ src/main/java/com/pelatihan/pelatihan/config/EmailConfig.java
@@ -2,6 +2,7 @@ package com.pelatihan.pelatihan.config;
 
 import java.util.Properties;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -9,10 +10,34 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class EmailConfig {
+
+    @Value("${mail.host}")
+    private String mailHost;
+
+    @Value("${mail.port}")
+    private String mailPort; 
+
+    @Value("${mail.username}")
+    private String mailUsername;
+
+    @Value("${mail.password}")
+    private String mailPassword;
+
+    @Value("${mail.protocol}")
+    private String mailProtocol;
+
+    @Value("${mail.starttls}")
+    private String mailStarttls;
+
+    @Value("${mail.auth}")
+    private String mailAut;
+
+    @Value("${mail.debug}")
+    private String mailDebug;
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
+        mailSender.setHost(mailHost); //ini server email
         mailSender.setPort(587);
         
         mailSender.setUsername("my.gmail@gmail.com");
