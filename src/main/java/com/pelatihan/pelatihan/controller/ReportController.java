df15bbf9313bc95e1ffd0ae94ee7e11c2bf640e3
Arya Segara <aryasegara10@gmail.com>
2025-03-16 11:34:57 +0700

    pertemuan-13

--- src/main/java/com/pelatihan/pelatihan/controller/ReportController.java
+++ src/main/java/com/pelatihan/pelatihan/controller/ReportController.java
@@ -2,36 +2,30 @@ package com.pelatihan.pelatihan.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import com.pelatihan.pelatihan.dto.EmailDto;
 import com.pelatihan.pelatihan.dto.GenericResponse;
-import com.pelatihan.pelatihan.service.EmailService;
+import com.pelatihan.pelatihan.service.ReportService;
 
 @RestController
-@RequestMapping("/email")
-public class EmailController {
-
+@RequestMapping("/report")
+public class ReportController {
+    
     @Autowired
-    private EmailService emailService;
+    private ReportService reportService;
 
-    public EmailController(EmailService emailService) {
-        this.emailService = emailService;
+    public ReportController(ReportService reportService) {
+        this.reportService = reportService;
     }
     
-    @PostMapping("/send")
-    public ResponseEntity<Object> sendEmail(@RequestBody EmailDto dto){
+     @GetMapping("/generateExcel")
+    public ResponseEntity<Object> generateExcel(){
         try {
-            emailService.sendEmail(dto.getTo(), dto.getSubject(), dto.getBody());
-            return ResponseEntity.ok().body(GenericResponse.builder()
-                                        .success(true)
-                                        .message("Successfully send Email")
-                                        .data(null)
-                                        .build());
+            return ResponseEntity.ok()
+                                    .body(reportService.generateExcel());
 
         } catch (ResponseStatusException rse) {
             return ResponseEntity.status(rse.getStatusCode())
@@ -50,5 +44,4 @@ public class EmailController {
                                         .build());
         }
     }
-
 }
--- src/main/java/com/pelatihan/pelatihan/service/ReportServiceImpl.java
+++ src/main/java/com/pelatihan/pelatihan/service/ReportServiceImpl.java
@@ -1,15 +1,100 @@
 package com.pelatihan.pelatihan.service;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
+import com.pelatihan.pelatihan.model.Users;
+import com.pelatihan.pelatihan.repository.UsersRepository;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
 public class ReportServiceImpl implements ReportService{
+
+    private final UsersRepository usersRepository;
+
+    public ReportServiceImpl(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
     
     @Override
-    public Object generateExcel() {
-        return null;
+    public Object generateExcel() throws IOException{
+        Workbook workbook = new XSSFWorkbook();
+
+        Sheet sheet = workbook.createSheet("DATA USER");
+
+        CellStyle headerStyle = workbook.createCellStyle();
+        headerStyle.setFillForegroundColor(IndexedColors.LIGHT_BLUE.getIndex());
+        headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+        XSSFFont headerFont = ((XSSFWorkbook) workbook).createFont();
+        headerFont.setFontName("Arial");
+        headerFont.setFontHeightInPoints((short) 12);  
+        headerFont.setBold(true);
+        headerFont.setColor(IndexedColors.WHITE.getIndex());
+
+        headerStyle.setFont(headerFont);
+
+
+        Row headerRow = sheet.createRow(0);
+
+        Cell idCell = headerRow.createCell(0);
+        idCell.setCellValue("ID");
+        idCell.setCellStyle(headerStyle);
+
+        Cell usernameCell = headerRow.createCell(1);
+        usernameCell.setCellValue("USERNAME");
+        usernameCell.setCellStyle(headerStyle);
+
+        Cell passwordCell = headerRow.createCell(2);
+        passwordCell.setCellValue("Password");
+        passwordCell.setCellStyle(headerStyle);
+
+        Cell statusCell = headerRow.createCell(3);
+        statusCell.setCellValue("Status");
+        statusCell.setCellStyle(headerStyle);
+
+        List<Users> usersList = usersRepository.findAll();
+
+        int currentIndexRow = 1;
+        for(Users user : usersList) {
+            Row bodyRow = sheet.createRow(currentIndexRow);
+
+            Cell cell = bodyRow.createCell(0);
+            cell.setCellValue(user.getId());
+
+            cell = bodyRow.createCell(1);
+            cell.setCellValue(user.getUsername());  
+
+            cell = bodyRow.createCell(2);
+            cell.setCellValue(user.getPassword());
+
+            cell = bodyRow.createCell(3);
+            cell.setCellValue(user.getStatus());
+
+            currentIndexRow++;
+        }
+
+        ByteArrayOutputStream  outputStream = new ByteArrayOutputStream();
+        try{
+            workbook.write(outputStream);
+            return outputStream.toByteArray();
+        }finally{
+            workbook.close();
+        }
+
+        return outputStream.toByteArray();
     }
 }
